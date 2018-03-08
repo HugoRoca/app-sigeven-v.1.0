aad01bf57d8ea1b0f4cdb4159c069a807a4cef32
Hugo Roca <hugo.rock20@hotmail.com>
2018-03-08 17:09:54 -0500

    cambios ventas

--- App/private/venta/venta.controller.js
+++ App/private/venta/venta.controller.js
@@ -11,16 +11,20 @@
     var vm = this;
     vm.fecha;
     vm.Ventas = {
-      cDescripcion:'',
+      cDescripcion: '',
       nArticulo: '',
       nPrecioVenta: 0,
-      nCantidad: 0
+      nCantidad: 0,
+      nCantidadTotal: 0,
+      nTotalVenta: 0,
+      nStock: 0
     }
 
     vm.Articulo;
 
     vm.buscaPrecio = buscaPrecio;
     vm.agregaFila = agregaFila;
+    vm.eliminarFila = eliminarFila;
 
     activate();
 
@@ -48,11 +52,13 @@
     }
 
     function buscaPrecio() {
+      vm.Ventas.nPrecioVenta = 0;
       for (let index = 0; index < vm.Articulo.length; index++) {
         const element = vm.Articulo[index];
         if (element.Id == vm.Ventas.nArticulo) {
           vm.Ventas.nPrecioVenta = element.nPrecioVenta;
           vm.Ventas.cDescripcion = element.cDescripcion;
+          vm.Ventas.nStock = element.nStock;
           $('#txtCantidad').focus();
         }
       }
@@ -61,7 +67,6 @@
     function cargaComboArticulo() {
       dataService.getData('Server/articulo_lista.php').then(function (data) {
         vm.Articulo = data.data;
-        console.log(data.data);
         //$('#cmbArticulos').selectpicker('render');
       }, function (error) {
         console.log(error);
@@ -69,28 +74,46 @@
     }
 
     function agregaFila() {
-      var table = document.getElementById("tblVentas");
 
-      var row = document.createElement("tr");
-      var td1 = document.createElement("td");//ID
-      var td2 = document.createElement("td");//Articulo
-      var td3 = document.createElement("td");//Precio
-      var td4 = document.createElement("td");//Cantidad
-      var td5 = document.createElement("td");//Total
+      if (vm.Ventas.nPrecioVenta == 0) return toastr.warning('Precio de venta cero, no se puede agregar.', 'Validación');
+      if (vm.Ventas.nCantidad == 0) return toastr.warning('Cantidad cero, no se puede agregar.', 'Validación');
+      if (vm.Ventas.nCantidad > vm.Ventas.nStock) return toastr.warning('Stock insuficiente, no se puede agregar.','Validación');
 
-      td1.innerHTML = vm.Ventas.nArticulo;
-      td2.innerHTML = vm.Ventas.cDescripcion;
-      td3.innerHTML = vm.Ventas.nPrecioVenta;
-      td4.innerHTML = vm.Ventas.nCantidad;
-      td5.innerHTML = parseInt(vm.Ventas.nCantidad) * parseFloat(vm.Ventas.nPrecioVenta);
+      var cols = '';
+      var total = Math.round(parseInt(vm.Ventas.nCantidad) * parseFloat(vm.Ventas.nPrecioVenta) * 100.00) / 100.00;
 
-      row.appendChild(td1);
-      row.appendChild(td2);
-      row.appendChild(td3);
-      row.appendChild(td4);
-      row.appendChild(td5);
+      cols += '<tr><td class="text-center"><input type="checkbox" name="record"></td>';
+      cols += '<td class="text-right">' + vm.Ventas.nArticulo + '</td>';
+      cols += '<td>' + vm.Ventas.cDescripcion + '</td>';
+      cols += '<td class="text-right">S/ ' + vm.Ventas.nPrecioVenta + '</td>';
+      cols += '<td class="text-right">' + vm.Ventas.nCantidad + '</td>';
+      cols += '<td class="text-right">S/ ' + total + '</td></tr>';
 
-      table.children[0].appendChild(row);
+      vm.Ventas.nCantidadTotal += vm.Ventas.nCantidad;
+      vm.Ventas.nTotalVenta += total;
+
+      //newRow.append(cols);
+      $('table tbody').append(cols);
+
+      vm.Ventas.nPrecioVenta = 0;
+      vm.Ventas.cDescripcion = '';
+      vm.Ventas.nCantidad = 0;
+      vm.Ventas.nArticulo = '';
+    }
+
+    function eliminarFila() {
+      $("table tbody").find('input[name="record"]').each(function () {
+        if ($(this).is(":checked")) {
+          var cantidad = parseInt($("td", $(this).parents("tr")).eq(4).text());
+          var total = $("td", $(this).parents("tr")).eq(5).text();
+          total = total.replace('S/ ', '');
+
+          vm.Ventas.nCantidadTotal -= cantidad;
+          vm.Ventas.nTotalVenta -= parseFloat(total);
+
+          $(this).parents("tr").remove();
+        }
+      });
     }
   }
 })();
