bac409416fd48d1aae02875cb00830984c16ecef
Hugo Roca <hugo.rock20@hotmail.com>
2018-03-13 13:44:15 -0500

    cambios proveedor

--- App/private/portal/portal.controller.js
+++ App/private/portal/portal.controller.js
@@ -9,6 +9,7 @@
 
   function portalController(configService, authenticationService, localStorageService, dataService, toastr, $state) {
     var vm = this;
+    vm.mostrar = true;
 
     init();
 
@@ -17,43 +18,50 @@
       listaTopVenta();
     }
 
-    function listaTopVenta(){
-      dataService.getData('Server/venta_listaPorSemana.php').then(function(data){
-        Morris.Bar({
-          element: 'ventaSemana',
-          data: data.data,
-          xkey: 'y',
-          ykeys: ['a', 'b'],
-          labels: ['Ventas', 'Gastos'],
-          hideHover: 'auto',
-          resize: true
+    function listaTopVenta() {
+      dataService.getData('Server/venta_listaPorSemana.php').then(function (data) {
+        if (data.data.length > 0) {
+          Morris.Bar({
+            element: 'ventaSemana',
+            data: data.data,
+            xkey: 'y',
+            ykeys: ['a', 'b'],
+            labels: ['Ventas', 'Gastos'],
+            hideHover: 'auto',
+            resize: true
+          });
+        } else {
+          toastr.warning('No hay datos', 'Dashboard');
+        }
+      }, function (error) {
+        console.log(error);
       });
-
-      }, function(error){console.log(error);});
     }
 
     function listaTopArticulo() {
       dataService.getData('Server/articulo_top.php').then(function (data) {
         var data = data.data;
-        var plotObj = $.plot($("#articulosMasVendidos"), data, {
-          series: {
-            pie: {
-              show: true
-            }
-          },
-          grid: {
-            hoverable: true
-          },
-          tooltip: true,
-          tooltipOpts: {
-            content: "%p.0%, %s",
-            shifts: {
-              x: 50,
-              y: 0
+        if (data.length > 0) {
+          var plotObj = $.plot($("#articulosMasVendidos"), data, {
+            series: {
+              pie: {
+                show: true
+              }
             },
-            defaultTheme: false
-          }
-        });
+            grid: {
+              hoverable: true
+            },
+            tooltip: true,
+            tooltipOpts: {
+              content: "%p.0%, %s",
+              shifts: {
+                x: 50,
+                y: 0
+              },
+              defaultTheme: false
+            }
+          });
+        }
       }, function (error) {
         console.log(error);
       });
